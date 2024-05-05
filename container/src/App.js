a9c4c9ffee33078d2abc5b08e277ac1a5f2cca07
patelsaheb1219 <kaumik1219@gmail.com>
2024-05-05 15:56:23 -0400

    Added Header component

--- container/src/App.js
+++ container/src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 
 const App = () => {
-  return <MarketingApp />;
+  return (
+    <BrowserRouter>
+      <Header />
+      <MarketingApp />
+    </BrowserRouter>
+  );
 };
 
 export default App;
