91395839981e39c6a09d6ee3f1a259622874f974
patelsaheb1219 <kaumik1219@gmail.com>
2024-05-07 21:30:37 -0400

    Implemented history connection between container and marketing app

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,12 +1,26 @@
 import { mount } from "marketing/MarketingApp";
 import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 const MarketingApp = () => {
-  useEffect(() => {
-    mount(ref.current);
-  });
 
   const ref = useRef(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    mount(ref.current, {
+      onNavigate: (location) => {
+        const { pathname: nextPathName } = location;
+        const { pathname } = history.location;
+
+        if (pathname !== nextPathName) {
+          history.push(nextPathName);
+        }
+      },
+
+    });
+  });
+
 
   return <div ref={ref}></div>;
 };
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,16 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory } from "history";
 
 import App from "./App";
 
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, props) => {
+  const { onNavigate } = props;
+  const history = createMemoryHistory();
+ if (onNavigate) {
+   history.listen(onNavigate);
+ }
+
+  ReactDOM.render(<App history={history} />, el);
 };
 
 if (process.env.NODE_ENV === "development") {
   const el = document.querySelector("#_marketing-dev-root");
   if (el) {
-    mount(el);
+    mount(el, {});
   }
 }
 
