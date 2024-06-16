734ab78b180de54ea345e2b16ff4fbe527a4b2cb
patelsaheb1219 <kaumik1219@gmail.com>
2024-06-16 16:30:14 -0400

    Removed extra div tag

--- auth/src/App.js
+++ auth/src/App.js
@@ -15,20 +15,18 @@ const generateClassName = createGenerateClassName({
 const App = (props) => {
   const { history, onSignIn } = props;
   return (
-    <div>
-      <StylesProvider generateClassName={generateClassName}>
-        <Router history={history}>
-          <Switch>
-            <Route path={"/auth/signin"}>
-              <SignIn onSignIn={onSignIn} />
-            </Route>
-            <Route path={"/auth/signup"}>
-              <SignUp onSignIn={onSignIn} />
-            </Route>
-          </Switch>
-        </Router>
-      </StylesProvider>
-    </div>
+    <StylesProvider generateClassName={generateClassName}>
+      <Router history={history}>
+        <Switch>
+          <Route path={"/auth/signin"}>
+            <SignIn onSignIn={onSignIn} />
+          </Route>
+          <Route path={"/auth/signup"}>
+            <SignUp onSignIn={onSignIn} />
+          </Route>
+        </Switch>
+      </Router>
+    </StylesProvider>
   );
 };
 
